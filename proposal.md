f9e5888007b2bab49f3973185e8a94a5119b077b
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-05-07 13:35:15 -0600

    changed proposal

--- proposal.md
+++ proposal.md
@@ -1,6 +1,6 @@
 # Major Project Proposal
 
-## Description
+<!-- ## Description
 A multiplier game with snakes and ladders, where you are trying to get to the end before the other players.
 
 ## Needs to have list
@@ -20,4 +20,28 @@ A multiplier game with snakes and ladders, where you are trying to get to the en
 - music
 - sound affects
 - multiplayer
-- pause
+- pause -->
+
+## Description
+A dinosaur game where you are avoiding obstacles as you collect points with the amount of time the game runs and you dont hit an obstacles so you don't die.
+
+## Needs to have list
+- A randomized terrain
+- A dinosaur that can jump with space
+- Obstacles such as cactai
+- Different sized obstacles 
+- A score in the corner
+- Track high score 
+- Start screen
+- Loose screen
+- How to play
+
+
+## Nice to have list
+- Different background colours
+- you can crouch the dinosaur and add bird obstacle
+- As time goes own the game gets faster
+- Music
+- Sound affects
+- Pause
+- WASD and arrows to move also
--- sketch.js
+++ sketch.js
@@ -6,12 +6,101 @@
 // - describe what you did to take this project "above and beyond"
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+// let grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],];
 
-function draw() {
-  background(220);
+
+// let cellSize;
+// const GRID_SIZE = 10;
+
+// function setup() {
+//   //make the canvas the largest square that you can...
+//   if (windowWidth < windowHeight) {
+//     createCanvas(windowWidth, windowWidth);
+//   }
+//   else {
+//     createCanvas(windowHeight, windowHeight);
+//   }
 
   
-}
+//   //this is dumb -- should check if this is the right size!
+//   cellSize = height/grid.length;
+// }
+
+// function windowResized() {
+//   //make the canvas the largest square that you can...
+//   if (windowWidth < windowHeight) {
+//     resizeCanvas(windowWidth, windowWidth);
+//   }
+//   else {
+//     resizeCanvas(windowHeight, windowHeight);
+//   }
+
+//   cellSize = height/grid.length;
+// }
+
+// function draw() {
+//   background(220);
+//   displayGrid();
+//   displayNumbers();
+// }
+
+
+// function displayGrid() {
+//   for (let y = 0; y < grid.length; y++) {
+//     for (let x = 0; x < grid[y].length; x++) {
+//       fill("white");
+//       square(x * cellSize, y * cellSize, cellSize);
+//     }
+//   }
+// }
+
+// function displayNumbers(){
+//   //goes through each element in the grid array
+//   for(let y = 0; y< grid.length; y++){
+//     for(let x = 0; x< grid[y].length; x++){
+//       // constent variables for where x and y for the text are
+//       let xText = x * cellSize + cellSize/2;
+//       let yText = y * cellSize + cellSize/2;
+
+//       fill("black");
+//       //displays the number on the grid
+//       if(grid[y][x] === 1){
+//         text("1", xText , yText);
+//       }
+//       else if(grid[y][x] === 2){
+//         text("2", xText , yText);
+//       }
+//       else if(grid[y][x] === 3){
+//         text("3", xText , yText);
+//       }
+//       else if(grid[y][x] === 4){
+//         text("4", xText , yText);
+//       }
+//       else if(grid[y][x] === 5){
+//         text("5", xText , yText);
+//       }
+//       else if(grid[y][x] === 6){
+//         text("6", xText , yText);
+//       }
+//       else if(grid[y][x] === 7){
+//         text("7", xText , yText);
+//       }
+//       else if(grid[y][x] === 8){
+//         text("8", xText , yText);
+//       }
+//       else if(grid[y][x] === 9){
+//         text("9", xText , yText);
+      
+//       }
+//     }
+//   }
+// }
