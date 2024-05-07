72e54997d49bb9e29cd9ef83496e800e842df1ee
yassmin21 <66423533+yassmin21@users.noreply.github.com>
2024-05-07 14:10:32 -0600

    started with rect for character

--- proposal.md
+++ proposal.md
@@ -1,48 +1,27 @@
 # Major Project Proposal
 
-<!-- ## Description
-A multiplier game with snakes and ladders, where you are trying to get to the end before the other players.
-
-## Needs to have list
-- a grid with numbers
-- snakes go down
-- ladders go up
-- a randomized number dice 
-- start screen
-- win screen
-- how to play
-- different characters and colours
-
-
-## Nice to have list
-- different modes
-- an actual dice with animation
-- music
-- sound affects
-- multiplayer
-- pause -->
-
 ## Description
 A dinosaur game where you are avoiding obstacles as you collect points with the amount of time the game runs and you dont hit an obstacles so you don't die.
 
 ## Needs to have list
 - A dinosaur that can jump with space
 - Obstacles such as cactai
-- Different sized obstacles 
+
 - A background 
 - A score in the corner
 - Track high score 
 - Start screen
-- Loose screen
+- Lose screen
 - How to play
-
+- Music
+- Sound affects
 
 ## Nice to have list
 - Different background colours
 - The background moves
+- Different sized obstacles 
 - you can crouch the dinosaur and add bird obstacle
 - As time goes own the game gets faster
-- Music
-- Sound affects
+
 - Pause
 - WASD and arrows to move also
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// Snakes and Ladders
+// No Wifi Dinosour game
 // Yassmin Ibrahim
 // 5 / 3/ 2024
 //
@@ -6,101 +6,37 @@
 // - describe what you did to take this project "above and beyond"
 
 
-// let grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],];
+//make a object for character
 
+let x = 30;
+let y = 175;
 
-// let cellSize;
-// const GRID_SIZE = 10;
+let gravity = 0.3;
+let velocity = 0;
 
-// function setup() {
-//   //make the canvas the largest square that you can...
-//   if (windowWidth < windowHeight) {
-//     createCanvas(windowWidth, windowWidth);
-//   }
-//   else {
-//     createCanvas(windowHeight, windowHeight);
-//   }
+function setup(){
+  createCanvas(600, 250);
+}
 
-  
-//   //this is dumb -- should check if this is the right size!
-//   cellSize = height/grid.length;
-// }
+function draw(){
+  background("grey");
+  character();
+}
 
-// function windowResized() {
-//   //make the canvas the largest square that you can...
-//   if (windowWidth < windowHeight) {
-//     resizeCanvas(windowWidth, windowWidth);
-//   }
-//   else {
-//     resizeCanvas(windowHeight, windowHeight);
-//   }
+function character(){
+  velocity += gravity;
+  y += velocity;
 
-//   cellSize = height/grid.length;
-// }
+  if(y > height - 70){
+    //doesnt let the bird go down the screen 
+    y = height - 70;
+    velocity = 0;
+  }
 
-// function draw() {
-//   background(220);
-//   displayGrid();
-//   displayNumbers();
-// }
+  if(mouseIsPressed){
+    velocity -= gravity * 3; 
+  }
 
+  rect(x, y, 50, 50);
+}
 
-// function displayGrid() {
-//   for (let y = 0; y < grid.length; y++) {
-//     for (let x = 0; x < grid[y].length; x++) {
-//       fill("white");
-//       square(x * cellSize, y * cellSize, cellSize);
-//     }
-//   }
-// }
-
-// function displayNumbers(){
-//   //goes through each element in the grid array
-//   for(let y = 0; y< grid.length; y++){
-//     for(let x = 0; x< grid[y].length; x++){
-//       // constent variables for where x and y for the text are
-//       let xText = x * cellSize + cellSize/2;
-//       let yText = y * cellSize + cellSize/2;
-
-//       fill("black");
-//       //displays the number on the grid
-//       if(grid[y][x] === 1){
-//         text("1", xText , yText);
-//       }
-//       else if(grid[y][x] === 2){
-//         text("2", xText , yText);
-//       }
-//       else if(grid[y][x] === 3){
-//         text("3", xText , yText);
-//       }
-//       else if(grid[y][x] === 4){
-//         text("4", xText , yText);
-//       }
-//       else if(grid[y][x] === 5){
-//         text("5", xText , yText);
-//       }
-//       else if(grid[y][x] === 6){
-//         text("6", xText , yText);
-//       }
-//       else if(grid[y][x] === 7){
-//         text("7", xText , yText);
-//       }
-//       else if(grid[y][x] === 8){
-//         text("8", xText , yText);
-//       }
-//       else if(grid[y][x] === 9){
-//         text("9", xText , yText);
-      
-//       }
-//     }
-//   }
-// }
